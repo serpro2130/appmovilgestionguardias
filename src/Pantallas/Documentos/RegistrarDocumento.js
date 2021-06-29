93eddb85ceea745fb8d4ae9e06c23e0932b7e786
SERPROEMCAM <serproemcamseguridadprivada@gmail.com>
2021-06-29 05:32:26 -0500

    Selección documentos - institción

--- src/Pantallas/Documentos/RegistrarDocumento.js
+++ src/Pantallas/Documentos/RegistrarDocumento.js
@@ -13,6 +13,7 @@ import { useNavigation } from "@react-navigation/native";
 import Loading from "../../Components/Loading";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import { addRegistro, ObtenerUsuario } from "../../Utils/Acciones";
+import { Picker } from "native-base";
 
 export default function RegistrarDocumento() {
   const [nombreDocumento, setNombreDocumento] = useState("");
@@ -92,18 +93,120 @@ export default function RegistrarDocumento() {
           alignSelf: "center",
         }}
       />
-      <Input
+      {/* <Input
         placeholder="Nombre Documento"
         onChangeText={(text) => setNombreDocumento(text)}
         inputStyle={styles.input}
         errorMessage={errores.nombreDocumento}
-      />
-      <Input
+      /> */}
+      <View
+        style={{
+          width: "90%",
+          height: 90,
+          alignItems: "center",
+          flexDirection: "row",
+        }}
+      >
+        <Text
+          style={{
+            width: "40%",
+            fontSize: 16,
+            fontStyle: "italic",
+            marginLeft: 5,
+          }}
+        >
+          Seleccione documento
+        </Text>
+        <Picker
+          note
+          mode="dialog"
+          style={{ width: 100, height: 60 }}
+          // selectedValue={hora}
+          onValueChange={(value) => setNombreDocumento(value)}
+        >
+          <Picker.Item label="Escoja" value="0" />
+          <Picker.Item
+            label="Permiso de operaciones"
+            value="Permiso de operaciones"
+          />
+          <Picker.Item label="Tenencia de armas" value="Tenencia de armas" />
+          <Picker.Item
+            label="Permiso de uniformes"
+            value="Permiso de uniformes"
+          />
+          <Picker.Item label="Matrícula de armas" value="Matrícula de armas" />
+          <Picker.Item
+            label="Autorización de funcionamiento"
+            value="Autorización de funcionamiento"
+          />
+          <Picker.Item
+            label="Patente de funcionamiento"
+            value="Patente de funcionamiento"
+          />
+          <Picker.Item
+            label="Nombramiento de gerente y presidente"
+            value="Nombramiento de gerente y presidente"
+          />
+          <Picker.Item
+            label="Obligaciones tributarias"
+            value="Obligaciones tributarias"
+          />
+          <Picker.Item
+            label="Aportaciones al Seguro Social"
+            value="Aportaciones al Seguro Social"
+          />
+          <Picker.Item label="Pólizas" value="Pólizas" />
+          <Picker.Item label="Registros guardias" value="Registros guardias" />
+        </Picker>
+      </View>
+
+      {/* <Input
         placeholder="Nombre Institución"
         onChangeText={(text) => setNombreInstitucion(text)}
         inputStyle={styles.input}
         errorMessage={errores.nombreInstitucion}
-      />
+      /> */}
+      <View
+        style={{
+          width: "90%",
+          height: 90,
+          alignItems: "center",
+          flexDirection: "row",
+        }}
+      >
+        <Text
+          style={{
+            width: "40%",
+            fontSize: 16,
+            fontStyle: "italic",
+            marginLeft: 5,
+          }}
+        >
+          Seleccione institución
+        </Text>
+        <Picker
+          note
+          mode="dialog"
+          style={{ width: 100, height: 60 }}
+          // selectedValue={hora}
+          onValueChange={(value) => setNombreInstitucion(value)}
+        >
+          <Picker.Item label="Escoja" value="0" />
+          <Picker.Item label="COSP" value="COSP" />
+          <Picker.Item label="CCFFAA" value="CCFFAA" />
+          <Picker.Item
+            label="Ministerio del Trabajo"
+            value="Ministerio del Trabajo"
+          />
+          <Picker.Item label="Municipio" value="Municipio" />
+          <Picker.Item label="Notaria" value="Notaria" />
+          <Picker.Item label="SRI" value="SRI" />
+          <Picker.Item label="IESS" value="IESS" />
+          <Picker.Item label="Seguros" value="Seguros" />
+          <Picker.Item label="POFASA" value="POFASA" />
+        </Picker>
+      </View>
+
       <Input
         placeholder="Presentar dd/mm/yyyy"
         onChangeText={(text) => setFechaPresentacion(text)}
